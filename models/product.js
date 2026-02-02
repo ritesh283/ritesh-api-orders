f6e9fe8fd10106767a30be30659ddd008800d555
Ritesh Rana <riteshxy1000@gmail.com>
2026-02-02 21:41:20 +0530

    update processing

--- models/product.js
+++ models/product.js
@@ -44,7 +44,7 @@ const productSchema = new mongoose.Schema({
         },
         orderstatus: {
           type:String,
-          default:"Order Proccessing"
+          default:"Order Processing"
           
         },
         details: {
